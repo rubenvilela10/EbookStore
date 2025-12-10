9c7367e56cc0b424f41e4f2fd00e825f3e0d7082
Ruben Vilela <r.vilela@runtime-revolution.com>
2025-12-10 16:32:45 +0000

    style: update signup and login forms for consistency and improve navbar layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user =User.find_by(email: params[:email].downcase)
 
-    if user&.autheticate(params[:password])
+    if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: "Logged in!"
     else
