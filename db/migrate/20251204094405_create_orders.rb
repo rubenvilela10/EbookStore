61131cfaa8bd9684b4b9251434ac0994d633e5f1
Ruben Vilela <r.vilela@runtime-revolution.com>
2025-12-11 12:07:25 +0000

    chore: standardize formatting in Gemfile and migration files by adding missing newlines and adjusting spacing for foreign key references

--- db/migrate/20251204094405_create_orders.rb
+++ db/migrate/20251204094405_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[8.1]
   def change
     create_table :orders do |t|
-      t.references :buyer,              null: false, foreign_key: {to_table: :users }
+      t.references :buyer,              null: false, foreign_key: { to_table: :users }
       t.references :ebook,              null: false, foreign_key: true
       t.float :price,                   null: false, default: 0.0
       t.float :fee,                     null: false, default: 0.0
